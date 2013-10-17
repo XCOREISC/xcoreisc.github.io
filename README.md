014b1f6f57e577d513c94ce541a1f25fe5aa2e2c
skeiter9 <skeiter97@gmail>
2013-10-17 11:53:03 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Requirements:
+
+Stylus
+
+Source Tree:
+ -images
+ -js
+ 	--lib
+ 	app.js
+ -styl
+ 	--__constants
+ 	--__components
+ 		---fonts
+ index.html
+ README.md
---
+++ js/app.js
@@ -0,0 +1,53 @@
+$(document).on("ready",function(){
+	$('#countdown').countdown( {date: '20 november 2013 12:24:00'} );
+	$("#days").knob({
+    min: 0,
+    max: 60,
+    readOnly :true,
+    width:70,
+    height:70,
+    fgColor:"#ebedee",
+    inputColor:"#ebedee",
+    thickness:0.2
+  });
+  $("#hours").knob({
+    min: 0,
+    max: 24,
+    readOnly :true,
+    width:70,
+    height:70,
+    fgColor:"#ebedee",
+    inputColor:"#ebedee",
+    thickness:0.2
+  });
+  $("#mins").knob({
+    min: 0,
+    max: 60,
+    readOnly :true,
+    width:70,
+    height:70,
+    fgColor:"#ebedee",
+    inputColor:"#ebedee",
+    thickness:0.2
+  });
+  $("#secs").knob({
+    min: 0,
+    max: 60,
+    readOnly :true,
+    width:70,
+    height:70,
+    fgColor:"#ebedee",
+    inputColor:"#ebedee",
+    thickness:0.2
+  });
+  /************** navigation ************/
+    var menuLeft = document.getElementById( 'nav' ),
+        buttonNav=document.getElementById('showMenu'),
+        body = document.body;
+      
+    buttonNav.onclick = function() {
+      classie.toggle( this, 'active' );
+      classie.toggle( body, 'menu-push-toright' );
+      classie.toggle( menuLeft, 'menu-open' );
+    };
+});
