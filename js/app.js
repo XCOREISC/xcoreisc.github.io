0df4d94bc7982d5e70307c1c523868a2bda76caf
skeiter9 <skeiter97@gmail.com>
2013-10-29 16:34:27 -0500

    first prototype

--- js/app.js
+++ js/app.js
@@ -1,3 +1,108 @@
+
+
+
+google.maps.event.addDomListener(window,'load',function(){
+
+
+    var styles = [
+      {
+        featureType: "landscape.man_made",
+        elementType: "geometry",
+        stylers: [
+          { color: "#e67e22" }
+        ]
+      },{
+        featureType: "landscape.natural.landcover",
+        elementType: "geometry",
+        stylers: [
+          { color: "#ff0000" }
+        ]
+      },{
+        featureType: "poi.business",
+        elementType: "geometry",
+        stylers: [
+          { color: "#66CBFF" }
+        ]
+      },{
+        featureType: "poi.attraction",
+        elementType: "geometry",
+        stylers: [
+          { color: "#fff55b" }
+        ]
+      },{
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          { color: "#1abc9c" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { color: "#7f8c8d" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          { color: "#ffffff" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "labels.text.stroke",
+        stylers: [
+          { color: "7f8c8d" }
+        ]
+      }
+
+    ];
+    var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
+
+    var mapOptions = {
+      zoom: 18,
+      center: new google.maps.LatLng(-6.704695,-79.901065),
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      }
+    };
+    var map = new google.maps.Map(document.getElementById('map-unprg'), mapOptions);
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
+    var infoWindow = new google.maps.InfoWindow({
+      content:"<a href='https://foursquare.com/YetsuPe'><span class='icon-home icon-2x'></span></a><p>YetsuPe</p>"
+    });
+
+    // var image = 'images/icons/yetsu-mark-map.png';
+     var myLatLng = new google.maps.LatLng(-6.704695,-79.901065);
+      var beachMarker = new google.maps.Marker({
+          position: myLatLng,
+          map: map
+      });
+
+    google.maps.event.addListener( beachMarker,'click',function(){
+      infoWindow.open(map,beachMarker);
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).on("ready",function(){
 	$('#countdown').countdown( {date: '20 november 2013 12:24:00'} );
 	$("#days").knob({
@@ -80,7 +185,7 @@ $(document).on("ready",function(){
   });
   iconPlace.on("click",function(){
     $(".navbar").css({"height":contentPlace.height()+'px'});
-    $(".cronograma").addClass("margin-top-3");
+    $(".cronograma").css({"margin-top":"1rem" } );
   });
   iconDate.on("click",function(){
     $(".navbar").css({"height":contentDate.height()+'px'});
@@ -88,11 +193,44 @@ $(document).on("ready",function(){
   });
   iconGallery.on("click",function(){
     $(".navbar").css({"height":contentGallery.height()+'px'});
-    $(".cronograma").addClass("margin-top-3");
+    $(".cronograma").css({"margin-top":"1rem" } );
   });
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
